8e1360ee9ed79ee9c95e10352bf70281ff277234
binkybear <nethunteros@users.noreply.github.com>
2016-10-27 21:38:46 -0500

    Copy to /system/xbin

--- Android.mk
+++ Android.mk
@@ -1,10 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := hidkeyboard
+LOCAL_MODULE := hid-keyboard
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TARGET_ARCH := arm
 LOCAL_MODULE_TARGET_ARCH_WARN := arm
+LOCAL_MODULE_PATH := $(TARGET_OUT)/xbin
 LOCAL_SRC_FILES_arm := arm/hid-keyboard
 include $(BUILD_PREBUILT)
